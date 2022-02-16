b94a3ebf074698ba33f6fa565f154f44a8313166
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-02-16 13:44:14 +0530

    [shweta]Add new contact

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -12,19 +12,8 @@ namespace AddressBook
         {
             Console.WriteLine("Welcome to Address Book Program");
 
-            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
-            AddressBookCreate addressBook = new AddressBookCreate()  //Initializing elements using collection-initializer syntax
-            {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                Zip = Convert.ToDouble(Console.ReadLine()),
-                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
-                Email = Console.ReadLine(),
-            };
-            addressBook.displayContact();
+            AddressBookCreate addressBook = new AddressBookCreate();
+            addressBook.AddContact();
             Console.ReadLine();
         }       
     }
