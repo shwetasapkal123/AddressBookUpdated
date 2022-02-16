d4174663c9b91060b88f5868c8cf9c976126d3a7
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-02-16 13:23:11 +0530

    [shweta]Ability to create Contact or address book

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -11,7 +11,21 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
-           Console.ReadLine();
-        }
+
+            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+            AddressBookCreate addressBook = new AddressBookCreate()  //Initializing elements using collection-initializer syntax
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToDouble(Console.ReadLine()),
+                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+            addressBook.displayContact();
+            Console.ReadLine();
+        }       
     }
 }
