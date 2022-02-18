2ca74efc8b76ed6168b75828fcf7a4267f40d1cf
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-02-18 17:20:44 +0530

    [shweta] Uploaded prgrm for no duplicate person in addressBook

---
+++ AddressBook/AddressBook/MainAddressBookof Contact.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class MainAddressBookof_Contact
+    {
+        //Collection Class
+        private List<Contact> contactList;
+        //Constructor.
+        public MainAddressBookof_Contact()
+        {
+            this.contactList = new List<Contact>();
+        }
+        //Method to Add Contact
+        public void AddContactDetails(string firstName, string lastName, string address, string city, string state, long zipCode, long phoneNumber, string email)
+        {
+
+            // finding the data that already has the same first name
+            Contact contact = this.contactList.Find(x => x.firstName.Equals(firstName));
+            // if same name is not present then add into address book
+            if (contact == null)
+            {
+                Contact contactDetails = new Contact(firstName, lastName, address, city, state, zipCode, phoneNumber, email);
+                this.contactList.Add(contactDetails);
+            }
+            // print person already exists in the address book
+            else
+            {
+                Console.WriteLine("Person, {0} is already exist in the address book", firstName);
+            }
+        }
+        //Display Contact
+        public void DisplayContact()
+        {
+            //cheks if ContactList is empty or not.
+            if (this.contactList.Count != 0)
+            {
+                foreach (Contact data in this.contactList)
+                {
+                    data.Display();
+                }
+            }
+            else
+                Console.WriteLine("No Contacts in AddressBook \n");
+        }
+        //Method to Edit Contact 
+        public void EditContact(string name)
+        {
+            // checks for every object whether the name is equal the given name
+            foreach (Contact data in this.contactList)
+            {
+                if (data.firstName.Equals(name))
+                {
+                    Console.WriteLine("Enter your choice:");
+                    Console.WriteLine("1. Last Name");
+                    Console.WriteLine("2. Address");
+                    Console.WriteLine("3. City");
+                    Console.WriteLine("4. State");
+                    Console.WriteLine("5. Zip");
+                    Console.WriteLine("6. Phone Number");
+                    Console.WriteLine("7. Email");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            data.lastName = Console.ReadLine();
+                            Console.WriteLine("Data updated successfully \n");
+                            break;
+                        case 2:
+                            data.address = Console.ReadLine();
+                            Console.WriteLine("Data updated successfully \n");
+                            break;
+                        case 3:
+                            data.city = Console.ReadLine();
+                            Console.WriteLine("Data updated successfully \n");
+                            break;
+                        case 4:
+                            data.state = Console.ReadLine();
+                            Console.WriteLine("Data updated successfully \n");
+                            break;
+                        case 5:
+                            data.zipCode = Convert.ToInt64(Console.ReadLine());
+                            Console.WriteLine("Data updated successfully \n");
+                            break;
+                        case 6:
+                            data.phoneNumber = Convert.ToInt64(Console.ReadLine());
+                            Console.WriteLine("Data updated successfully \n");
+                            break;
+                        case 7:
+                            data.email = Console.ReadLine();
+                            Console.WriteLine("Data updated successfully \n");
+                            break;
+                        default:
+                            Console.WriteLine("Enter Valid Choice! \n");
+                            break;
+                    }
+                }
+                else
+                    Console.WriteLine("No Contact With this Name! \n");
+            }
+        }
+        //Method to Delete a Person
+        public void DeleteContact(string dName)
+        {
+            foreach (Contact ct in this.contactList)
+            {
+                if (ct.firstName.Equals(dName))
+                {
+                    this.contactList.Remove(ct);
+                    Console.WriteLine("Contact Deleted! \n");
+                    break;
+                }
+            }
+        }
+    }
+}
